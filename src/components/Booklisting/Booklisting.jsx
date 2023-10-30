3792b2a441d353f71845998f2e0f389c70655ac4
Jay Woolcott-White <jay.lww@icloud.com>
2023-10-30 12:21:35 +0000

    made the fetch request for all books past the props into book listings

--- src/components/Booklisting/Booklisting.jsx
+++ src/components/Booklisting/Booklisting.jsx
@@ -1,7 +1,13 @@
 // booklisting will take props to display JSON data from Home 
-function BookListing(){
-    <>
-    <p>this is a book</p>
-    </>
+function BookListing({title, author, image, genre}){
+
+    return(
+        <>
+        <h1>{title}</h1>
+        <h2>{author}</h2>
+        <img src={image} alt={title}/>
+        <p>{genre}</p>
+        </>
+    )
 }
 export default BookListing
