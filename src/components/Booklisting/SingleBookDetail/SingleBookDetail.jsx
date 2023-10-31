98c32fbc4d8990c24109074dc74902a7a71ea1ab
Michael Bertram <michael@bigbite.net>
2023-10-31 09:31:11 +0000

    Additional styling to SingleBookDetail page and nav bar

--- src/components/Booklisting/SingleBookDetail/SingleBookDetail.jsx
+++ src/components/Booklisting/SingleBookDetail/SingleBookDetail.jsx
@@ -31,17 +31,18 @@ function SingleBookDetail () {
     }, [])
     
     return (
-        <div className ="display_container">
-            <div className="display_container image">
+        <div className= "display_container">
+            <div className= "display_container image">
               <img src={image} alt={title} />
             </div>
-            <div className="display_container content">
+            <div className= "display_container content">
               <h1>{title}</h1>
-              <h2>{author}</h2>
-              <p>{year}</p>
-              <p>{pageCount}</p>
-              <p>{genre}</p>
-              <p>{blurb}</p>
+              <p><strong>Author:</strong> {author}</p>
+              <p><strong>Published:</strong> {year}</p>
+              <p><strong>Pages:</strong> {pageCount}</p>
+              <p><strong>Genre:</strong> {genre}</p>
+              <p><strong>Blurb:</strong></p>
+              <p className="blurb">{blurb}</p>
             </div>
         </div>
     )
