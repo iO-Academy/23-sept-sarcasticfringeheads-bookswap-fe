5d7d0fba1f57276561584c2c58e3b66f08b538d0
Michael Bertram <michael@bigbite.net>
2023-11-02 19:58:25 +0000

    Added welcome section under the nav bar

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -38,11 +38,14 @@ function Home() {
     return (
         <>
            <div className="welcome">
-
+            <h1>Welcome to </h1>
+            <h2>Book Swap</h2>
+            <h3>Checkout available books, whats claimed and add a book</h3>
+            <span></span>
             
             </div> 
             
-            
+    
             <div className="filter">
                 <label > Filter by genre:</label>
             
