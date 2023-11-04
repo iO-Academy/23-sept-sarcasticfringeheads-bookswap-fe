23c0253ba5a6aba8353b67702cb46a3b5f8d3fd2
Rob Sheldrick <lebaguettemuncherer@gmail.com>
2023-11-04 22:39:53 +0000

    tried to add rating number of star icons to bookreviewpage component (each individual book review) but not working for now

--- src/components/BookReviewPage/BookReviewPage.jsx
+++ src/components/BookReviewPage/BookReviewPage.jsx
@@ -1,10 +1,25 @@
+import { useEffect, useState } from "react"
 import "./BookReviewPage.css"
-
+import { FaStar } from "react-icons/fa"
 function BookReviewPage({name, review, rating}){
+const [roundedReviews2, setRoundedReviews2] = useState([])
+    
+useEffect(() => {
+    let roundedRating = Math.round(rating)
+    console.log(roundedRating)
+    setRoundedReviews2(Array.apply(null, Array(Math.round(roundedRating))))
+    console.log(roundedReviews2)
+}, [])
+
     return (
         <div className="reviewBox">
             <h3>{name}</h3>
-            <h4>Score:&nbsp;{rating}/5 Stars&#9733;</h4>
+            <h4>Score:&nbsp;{rating}/5 Stars 
+            
+            {roundedReviews2.map(star => {
+                <FaStar />
+            })}
+            </h4>
             <p>{review}</p>
         </div>
     )}
